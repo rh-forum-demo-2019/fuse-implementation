c144003889bdd5d1314c8794323f1ecaf48de89f
Developer <developer@me.com>
2019-09-13 01:11:42 +0200

    first draft of a working pipeline

--- setup.sh
+++ setup.sh
@@ -2,4 +2,9 @@
 oc new-project beer-demo-qa
 oc new-project beer-demo-prod
 oc new-app fuse7-java-openshift:1.2~https://github.com/rh-forum-demo-2019/fuse-implementation.git -n beer-demo-qa
-
+oc expose service fuse-implementation -n beer-demo-qa
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n beer-demo-qa
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n beer-demo-prod
+oc new-app fuse-implementation -n beer-demo-prod
+oc expose svc fuse-implementation -n beer-demo-prod
+oc set triggers dc/fuse-implementation --manual -n beer-demo-prod
