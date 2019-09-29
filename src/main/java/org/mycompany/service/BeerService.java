883750c88dc4a691f37d6eee4c746171fa877ecc
Developer <developer@me.com>
2019-09-29 22:59:36 +0200

    added find beer by status

--- src/main/java/org/mycompany/service/BeerService.java
+++ src/main/java/org/mycompany/service/BeerService.java
@@ -3,6 +3,7 @@ package org.mycompany.service;
 import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.example.beer.dto.Beer;
 
@@ -20,6 +21,22 @@ public class BeerService {
         beer.setStatus("Available");
         beer.setType("IPA");
         mBeerList.add(beer);
+
+        beer = new Beer();
+        beer.setCountry("DK");
+        beer.setName("Wookiee IPA - Amager Bryghus");
+        beer.setRating(new BigDecimal(5));
+        beer.setStatus("UnAvailable");
+        beer.setType("IPA");
+        mBeerList.add(beer);
+
+        beer = new Beer();
+        beer.setCountry("DK");
+        beer.setName("Pilsner - Tuborg");
+        beer.setRating(new BigDecimal(3));
+        beer.setStatus("UnAvailable");
+        beer.setType("Pilsner");
+        mBeerList.add(beer);
     }
     public List<Beer> getBeers() {
         return mBeerList;
@@ -29,5 +46,10 @@ public class BeerService {
             .filter(beer -> beer.getName().startsWith(name))
             .findFirst()
             .orElse(null);
+    }
+    public List<Beer> getBeersByStatus(String status) {
+        return mBeerList.stream()
+            .filter(beer -> beer.getStatus().equals(status))
+            .collect(Collectors.toList());
     }
 }
