ffd608b6a2019795c705652af865bbbe81189c69
John Doe <developer@example.com>
2019-09-30 13:24:37 +0000

    list beers added

---
+++ src/main/java/org/mycompany/routes/GetBeersRoute.java
@@ -0,0 +1,29 @@
+package org.mycompany.routes;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.mycompany.service.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class GetBeersRoute extends RouteBuilder {
+
+  @Autowired
+  private BeerService mBeerService;
+
+  @Override
+  public void configure() throws Exception {
+    from("direct:ListBeers").log("ListBeers").process(new Processor() {
+
+      @Override
+      public void process(Exchange exchange) throws Exception {
+        exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 200);
+        exchange.getIn().setBody(mBeerService.getBeers());
+      }
+    }).marshal().json(JsonLibrary.Jackson);
+  }
+}
